a7e4ece459213920462b5bc6a0878b7475fd4e8a
Gabriel <gabri.com@live.com>
2021-03-11 14:36:58 -0300

    docker compose and kafka producer done

--- transaction-api/src/main/java/com/gabrielduarte/transactionapi/service/TransactionService.java
+++ transaction-api/src/main/java/com/gabrielduarte/transactionapi/service/TransactionService.java
@@ -4,22 +4,22 @@ import com.gabrielduarte.transactionapi.domain.Transaction;
 import com.gabrielduarte.transactionapi.mapper.TransactionMapper;
 import com.gabrielduarte.transactionapi.repository.TransactionRepository;
 import com.gabrielduarte.transactionapi.request.TransactionRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TransactionService {
 
     private final TransactionRepository repository;
     private final TransactionMapper mapper;
+    private final TransactionProducer producer;
 
-    public TransactionService(final TransactionRepository repository, final TransactionMapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
+    public void createTransaction(final TransactionRequest request) {
+        Transaction transaction = mapper.toEntity(request);
+        repository.save(transaction);
+
+        producer.sendMessageTransactionDone(transaction);
     }
 
-    public void createTransaction(final TransactionRequest transaction) {
-        Transaction entity = mapper.toEntity(transaction);
-
-        repository.save(entity);
-    }
 }
